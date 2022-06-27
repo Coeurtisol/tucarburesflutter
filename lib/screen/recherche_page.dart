94d398acf4ddbf992f5d95e4d389135bdfc09d02
danyrose <danyrose1995@gmail.com>
2022-06-27 11:48:52 +0200

    bottom navigation bar

---
+++ lib/screen/recherche_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RecherchePage extends StatelessWidget {
+  const RecherchePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("recherche"),
+    );
+  }
+}

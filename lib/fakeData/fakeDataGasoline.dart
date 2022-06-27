78c8d6b7f69e45405dad8442978b13d2b4cdc8e7
Lyz59 <amelie_du89@hotmail.fr>
2022-06-27 15:19:17 +0200

    model + fake data

---
+++ lib/fakeData/fakeDataGasoline.dart
@@ -0,0 +1,36 @@
+import '../model/gasoline.dart';
+
+const fakeGasoline = [
+  Gasoline(
+      id: '1',
+      gasStationId: "",
+      name: "Sans Plomb 98 (E5)",
+      code: "268-661",
+      price: "2,101"),
+  Gasoline(
+      id: '2',
+      gasStationId: "",
+      name: "Sans Plomb 95 (E5)",
+      code: "181-869",
+      price: "2,067"),
+  Gasoline(
+      id: '3',
+      gasStationId: "",
+      name: "Sans Plomb 95 (E10)",
+      code: "067-502",
+      price: "2,009"),
+  Gasoline(
+      id: "4",
+      gasStationId: "",
+      name: "Superéthanol E85",
+      code: "974-006",
+      price: "0,749"),
+  Gasoline(
+      id: "5",
+      gasStationId: "",
+      name: "Gazole (B7)",
+      code: "699-991",
+      price: "2,059"),
+  Gasoline(
+      id: "6", gasStationId: "", name: "GPL", code: "379-503", price: "0,749"),
+];
---
+++ lib/model/gasoline.dart
@@ -0,0 +1,17 @@
+
+
+class Gasoline {
+  final String id;
+  final String gasStationId;
+  final String name;
+  final String code;
+  final String price;
+
+  const Gasoline({
+    required this.id,
+    required this.gasStationId,
+    required this.name,
+    required this.code,
+    required this.price,
+  });
+}
